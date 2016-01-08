3222d0279d2140485bcedf1d0af182a128cd50f8
ayush <ayushkumarsingh97@gmail.com>
2016-01-08 17:23:39 +0530

    initial comit

---
+++ app.py
@@ -0,0 +1,129 @@
+from flask import Flask
+from flask import render_template,request,flash,url_for,redirect,session
+from content import *
+from wtforms import Form,BooleanField,TextField,PasswordField,validators
+from passlib.hash import sha256_crypt
+
+from copy import copy
+
+
+
+
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route("/user/<username>")
+def profile(username):
+	return render_template("profile.html",name = username)
+
+
+@app.route("/header/")
+def header():
+	return render_template("header.html")
+
+
+@app.route("/output/",methods=['GET','POST'])
+def output():
+	
+	
+
+	return render_template("output.html")
+
+@app.route("/solved_sud/",methods=["GET",'POST'])
+def solved():
+	try:
+		lst = [[],[],[],[],[],[],[],[],[]]
+		lstnew = [[],[],[],[],[],[],[],[],[]]
+		
+		count = 0
+		if request.method == "POST":
+			inp = request.form.getlist("num")
+			for i in range(0,81):
+				inp[i] = int(inp[i])
+			print inp
+			for r in range(0,9):
+				for c in range(0,9):
+					lst[r].append(inp[count])
+					count = count +1
+			print lst
+			import info
+			print info.grids
+			
+			
+			info.grids = lst
+			print info.grids
+
+			#print info.grids
+			import sudoku_solver 
+			sudoku_solver.grid = lst
+			print info.grids
+			print sudoku_solver.grid
+			sol = sudoku_solver.run()
+			print sol
+			print sudoku_solver.grid
+		
+			
+			
+
+			return render_template("solved_sud.html",lst=sol,inp = inp)
+
+			#return redirect(url_for("solved",lst = sol,name = 23))
+	except Exception as e:
+		print e.message
+
+
+#class Registrationform(Form):
+#	username = TextField("Username",[validators.Length(min=4,max = 20)])
+#	email = TextField("Email address",[validators.Length(min=6,max='50')])
+#	password = PasswordField('Password',[validators.Required(),
+#		validators.EqualTo('confirm',message="wrong ")])
+#	confirm = PasswordField('Repeat password')
+#	accept_tos  = BooleanField("i accept",[validators.Required()])
+
+
+
+#@app.route("/register/",methods = ["GET","POST"])
+#def registerpage():
+#	try:
+#		form = Registrationform(request.form)
+#		if request.method =="POST" and form.validate():
+#			username = form.username.data
+#			email = form.email.data
+#			password = sha256_crypt.encrypt((str(form.password.data)))
+#
+#		return render_template("register.html",form=form)
+#
+#		session["logged_in"] = True
+#		session["username"] = username
+#	except:
+#		pass
+
+
+
+#@app.route("/login/",methods = ['GET','POST'])
+#def login():
+#	error =None
+#	try:
+#		if request.method =="POST":
+#			attem_username = request.form["username"]
+#			attem_password = request.form["password"]
+#			writetext(attem_username)
+#			
+#
+#
+#			if attem_username == "admin" and attem_password =="password":
+#				return redirect(url_for('profile',username = attem_username))
+#			else:
+#				return redirect(url_for("index"))	
+#
+#		return render_template("login.html",error=error)		
+#	except:
+#		return redirect(url_for("aab",username ="asd"))	
+
+
+if __name__ == '__main__' :
+	app.run(debug=True)
+
---
+++ sudoku_solver.py
@@ -0,0 +1,94 @@
+import info
+
+
+    
+
+
+grid = info.grids	
+#grid = [[u'0', u'0', u'5', u'0', u'0', u'6', u'3', u'0', u'0'], [u'8', u'0', u'0', u'3', u'1', u'0', u'0', u'4', u'2   '], [u'0', u'0', u'1', u'0', u'0', u'0', u'0', u'0', u'7'], [u'0', u'0', u'4', u'0', u'0', u'9', u'0', u'0', u'8'], [u'6', u'0', u'0', u'0', u'0', u'5', u'7', u'0', u'0'], [u'9', u'0', u'0', u'0', u'0', u'0', u'0', u'0', u'0'], [u'0', u'0', u'0', u'5', u'0', u'0', u'0', u'0', u'0'], [u'2', u'0', u'0', u'0', u'0', u'0', u'0', u'0', u'5'], [u'0', u'0', u'3', u'0', u'0', u'1', u'2', u'0', u'0']]
+
+
+
+
+def solvesudoku():
+	global grid
+	rc = find_unassigned()
+	if rc == []:
+		return True
+	ROW = rc[0] 
+	COLMN = rc[1]
+	
+	for i in range(1,10):
+		if issafe(i,ROW,COLMN):
+			grid[ROW][COLMN] = i
+			
+
+			if solvesudoku():
+				
+				return True
+
+			grid[ROW][COLMN] = 0
+
+	return False          #backtrack when all possible numbers didny fit we make 
+
+
+
+
+def UsedinRow(num,row):
+	for j in range(0,9):
+		if num==grid[row][j]:
+			return True
+	return False
+def UsedinColm(num,colm):
+	for j in range(0,9):
+		if num==grid[j][colm]:
+			return True
+	return False
+def UsedinBlock(num,row,colm):
+	startrow = (row//3)*3
+	endrow = startrow+3
+	startcol = (colm//3)*3
+	endcol = startcol+3
+	for p in range(startrow,endrow):
+		for q in range(startcol,endcol):
+			if grid[p][q]==num:
+				return True
+	return False
+def issafe(num,row,colm):
+	return not UsedinRow(num,row) and not UsedinColm(num,colm) and not UsedinBlock(num,row,colm)
+
+
+def find_unassigned():
+	for row in range(0,9):
+		for colmn in range(0,9):
+			if grid[row][colmn]==0:
+				return [row,colmn]
+	return []
+
+def printsud():
+	for i in range(9):
+		for j in range(9):
+			print grid[i][j],
+		print
+
+def run():
+	for i in range(9):
+		for j in range(9):
+			grid[i][j] = int(grid[i][j])
+	
+	if solvesudoku():
+		
+		printsud()
+		return grid 
+
+	else:
+		print "not solvable"		
+
+
+
+
+
+
+
+
+
