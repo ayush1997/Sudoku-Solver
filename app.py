13a9e043ced3b2477b6f0e04d077c3daef086e47
ayush <ayushkumarsingh97@gmail.com>
2016-01-12 10:34:34 +0530

    .

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def solved():
 
 
 if __name__ == '__main__' :
-	port = int(os.environ.get('PORT', 5000))
+	#port = int(os.environ.get('PORT', 5000))
 	app.run(host='0.0.0.0',port=port)
 	#app.run(debug=True)
 
